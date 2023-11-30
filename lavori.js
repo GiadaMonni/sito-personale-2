c6b0d54b62b3fba61949fc44817e147e37cf9903
Giada Monni <gm.kyraraebrian@gmail.com>
2023-11-30 14:41:32 +0100

    creazione sito personale

---
+++ lavori.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var swiper = new Swiper(".mySwiper", {
+      speed: 2000,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+    });
+  });
+
+  
+
+
